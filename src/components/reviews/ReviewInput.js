ea420b3deacf50ce2f9502ae2d6d1028818619a8
Andrew Davisson <akdavisson4@gmail.com>
2019-11-04 10:17:48 -0700

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
-import Reviews from './Reviews';
+import React, { Component } from "react";
+import Reviews from "./Reviews";
 
 class ReviewInput extends Component {
+  state = {
+    text: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addReview(this.props.restaurantId, this.state.text);
+  };
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Review: </label>
+          <input
+            type="text"
+            name="text"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
-};
+}
 
 export default ReviewInput;
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react'
-import ReviewInput from '../components/reviews/ReviewInput'
-import Reviews from '../components/reviews/Reviews'
+import React, { Component } from "react";
+import ReviewInput from "../components/reviews/ReviewInput";
+import Reviews from "../components/reviews/Reviews";
+import { connect } from "react-redux";
 
 class ReviewsContainer extends Component {
-
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          restaurantId={this.props.restaurant.id}
+          addReview={this.props.addReview}
+        />
+        <Reviews
+          restaurant={this.props.restaurant}
+          reviews={this.props.reviews}
+          deleteReview={this.props.deleteReview}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addReview: (restaurantId, text) =>
+      dispatch({ type: "ADD_REVIEW", review: {restaurantId: restaurantId, text: text} }),
+    deleteReview: id => dispatch({ type: "DELETE_REVIEW", id: id })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ReviewsContainer);
