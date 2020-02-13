7fc41e17a0cbdeaf3f9f27f36f601c35a4dc99ab
Adil Naimi <adilnm23@hotmail.com>
2020-02-13 13:29:14 -0600

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state={
+    text:''
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      text:e.target.value
+    })
+  }
+  
+  handleSubmit=(e)=>{
+    e.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text:''
+    })
+  }
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" placeholder='Restaurant Name' value={this.state.text}/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
-
+import { connect } from 'react-redux'
 class ReviewsContainer extends Component {
 
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput restaurantId={this.props.restaurant.id} addReview={this.props.addReview}/>
+        <Reviews restaurantId={this.props.restaurant.id} reviews={this.props.reviews} deleteReview={this.props.deleteReview}/>
       </div>
     )
   }
 }
+const mstp=(state)=>{
+  return {
+    reviews:state.reviews
+  }
+}
+const mdtp=(dispatch)=>{
+  return {
+    addReview: review => dispatch({type: 'ADD_REVIEW', review}),
+    deleteReview: id => dispatch({type: 'DELETE_REVIEW', id})
+  }
+}
 
-export default ReviewsContainer
+export default connect(mstp,mdtp)(ReviewsContainer)
