8de3b120028580d30b69a733da6657ba6ed0d846
Jane Venter <janeanneventer@gmail.com>
2020-04-05 20:05:04 -0700

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+  listRestaurants = () => this.props.restaurants.map( restaurant => <Restaurant restaurant={restaurant} delete={this.props.delete} /> )
+
   render() {
+    //console.log("props: ", this.props)
     return(
       <ul>
         Restaurants Component
+        {this.listRestaurants()}
       </ul>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  constructor(props){
+    super(props);
+    //console.log("initialprops: ", props.restaurant.id)
+    this.state = {
+      text: ''
+    }
+  } 
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    console.log("props: ", this.props.restaurant.id)
+    event.preventDefault();
+    //this.props.addReview(...review, this.props.restaurant.id)
+    this.props.addReview( 
+      {text: this.state.text, restaurantId: this.props.restaurantId} 
+    )
+  }
+
   render() {
+    //id=id
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit}>
+            <input type="text" value={this.state.text} onChange={this.handleOnChange} />
+            <input type="submit" value="Submit"  />
+        </form>
       </div>
     );
   }
