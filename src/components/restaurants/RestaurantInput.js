776e6a346ae5fdc8aab1f3ccd5387f4cb7fbd755
jeremyamram <52257264+jeremyamram@users.noreply.github.com>
2020-04-19 18:14:17 +0100

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ""
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ""
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form
+          onSubmit={(e) => this.handleSubmit(e)}
+         >
+          <input 
+            type="text"
+            value={this.state.text}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 
 class ReviewsContainer extends Component {
 
   render() {
+    const { addReview, restaurant, reviews, deleteReview} = this.props;
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput 
+          addReview={addReview}
+          restaurantId={restaurant.id}
+        />
+        <Reviews
+          reviews={reviews}
+          restaurantId={restaurant.id}
+          deleteReview={deleteReview}
+        />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = ({reviews}) => {
+  return {reviews}
+}
+
+const mapDispatchToProps = dispatch => ({
+  addReview: review => dispatch({type: 'ADD_REVIEW', review}),
+  deleteReview: id => dispatch({type: 'DELETE_REVIEW', id})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
+
