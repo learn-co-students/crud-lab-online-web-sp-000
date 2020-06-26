8828aae5cc52927e133c2e7c8d675d71d31ef436
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-26 11:24:58 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,11 @@ class Reviews extends Component {
   render() {
     return (
       <ul>
-        Reviews
+        {this.props.reviews.map(review => {
+          return (
+            <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
+          )
+        })}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,25 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {restaurants: [], reviews: []}, action) {
+  let idx;
+  switch(action.type) {
 
+    case 'ADD_RESTAURANT':
+      return {...state, restaurants: [...state.restaurants, { text: action.rest, id: cuid() }]};
+
+    case 'DELETE_RESTAURANT':
+      idx = state.restaurants.findIndex(rest => rest.id === action.id);
+      return {...state, restaurants: [...state.restaurants.slice(0, idx), ...state.restaurants.slice(idx + 1)] }
+
+    case 'ADD_REVIEW':
+      return {...state, reviews: [...state.reviews, { text: action.review.text, id: cuid(), restaurantId: action.review.restaurantId }]};
+
+    case 'DELETE_REVIEW':
+      idx = state.reviews.findIndex(review => review.id === action.id);
+      return {...state, reviews: [...state.reviews.slice(0, idx), ...state.reviews.slice(idx + 1)] };
+
+    default:
+      return state;
+  }
 }
