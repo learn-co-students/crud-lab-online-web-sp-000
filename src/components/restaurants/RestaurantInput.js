9f49b684c15046288756b89371b42e35dfaea2a4
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-19 17:23:49 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange (event) {
+    this.setState({
+      text: event.target.value
+    });
+
+  }
+
+  handleOnSubmit (event) {
+    this.preventDefault();
+    this.props.delete(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+  
   render() {
+    
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+          <input onChange={(event) => this.handleChange(event)} >Restaurant Input</input>
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
 
@@ -8,10 +9,29 @@ class RestaurantsContainer extends Component {
     return (
       <div>
         <RestaurantInput />
-        <Restaurants />
+        <Restaurants  />
       </div>
     )
   }
 }
 
+
+// // const mapStateToProps = state => {
+// //   return {
+// //     restaurants: state.restaurants
+// //   }
+// }
+ 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     delete: text => dispatch({type: 'DELETE_RESTAURANT', payload: text })
+//   }
+// }
+
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer);
+
 export default RestaurantsContainer
+
+
