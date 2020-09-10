ead12180ca31140c25956caddcbb587e284fc95d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-10 23:11:38 +0200

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: "",
+    restaurantId: this.props.restaurantId
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      ...this.state,
+      text: e.target.value
+    })
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    this.props.addReview(this.state);
+    this.setState({
+      text: "",
+      restaurantId: this.props.restaurantId
+    })
+  }
+  
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" onChange={this.handleOnChange} value={this.state.text}/>
+          <input type="submit" value="submit"/>
+        </form>
       </div>
     );
   }
