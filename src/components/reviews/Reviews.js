a269c6fdee4d110970154159d6bbf2c62bab5293
Soner Mezgitci <mezgitci9@gmail.com>
2020-10-06 18:57:16 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,14 +3,15 @@ import Review from './Review';
 
 class Reviews extends Component {
 
-renderReviews = () => {
-  return this.props.reviews.map( review => {
-  if (review.id === this.props.restaurant.id)
-    <Review key={review.id} review={review} deleteReview={this.props.delete}/>
-    // review.id === this.props.resturant.id ? this.props.reviews: null
+  renderReviews = () => {
+    return this.props.reviews.map( review => {
+    if (review.restaurantId === this.props.restaurant.id) {
+      debugger;
+      return <Review key={review.id} review={review} deleteReview={this.props.delete}/>
+      // review.id === this.props.resturant.id ? this.props.reviews: null
+    }
+  })
   }
-  )
-}
   render() {
     console.log("REVIEWS",this.props.restaurant)
     return (
