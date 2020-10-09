fb93f546f904387c4b999c91be53918c72f50a9e
Alvin Coulson <aaacoulson@gmail.com>
2020-10-09 18:37:51 -0400

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer';
 
 class Restaurant extends Component {
-
+  handleClick = () => {
+    this.props.deleteRestaurant(this.props.restaurant.id);
+  }
 
   render() {
-    const { restaurant } = this.props;
-
     return (
       <div>
         <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+          {this.props.restaurant.text}
+          <button onClick={this.handleClick}> X </button>
+          <ReviewsContainer restaurant={this.props.restaurant} />
         </li>
       </div>
     );
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const restaurantReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId);
+    const reviewList = restaurantReviews.map(review => {
+      return <Review key={review.id} review={review} deleteReview={this.props.deleteReview}/>
+    })
+
     return (
       <ul>
-        Reviews
+        {reviewList}
       </ul>
     );
   }
