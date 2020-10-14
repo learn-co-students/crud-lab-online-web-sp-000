b6b12da8dd7459944bf3bf30c61d689d164e38e5
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-13 22:03:40 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { ProgressPlugin } from 'webpack';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+    const {restaurants, deleteRestaurant} = this.props
+    const showRestaurants = restaurants.map(restaurant => {
+      return (
+        <li>
+        <Restaurant 
+          key={restaurant.id}
+          restaurant={restaurant}
+          deleteRestaurant={deleteRestaurant}
+        />
+        </li>
+      )
+    })
+
     return(
       <ul>
-        Restaurants Component
+       {showRestaurants}
       </ul>
     );
   }
