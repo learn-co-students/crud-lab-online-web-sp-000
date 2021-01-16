5fe9893e1143b8b972ea26ede51d057689a1144f
Jasmine Carter <heyjasminec@gmail.com>
2021-01-16 17:14:19 -0500

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addReview({text:this.state.text, restaurantId: this.props.restaurantId})
+    this.setState({
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div>
         Review Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type="text"
+            name="review_input"
+            value={this.state.text}
+            onChange={this.handleOnChange}/>
+          <input
+            type="submit"
+            value="submit" />
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,23 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const restReviews = reviews.filter(review => review.restaurantId === restaurantId)
+
+    const allReviews = restReviews.map(review => {
+      return(
+        <li><Review
+        key={review.id}
+        review={review}
+        deleteReview={deleteReview}
+        />
+        </li>
+      )
+    })
     return (
       <ul>
         Reviews
+        {allReviews}
       </ul>
     );
   }
