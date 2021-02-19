1687eb51a7198ebcaa1489ae139adeb1bfd61225
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-02-19 01:34:51 -0800

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -6,12 +6,9 @@ class Reviews extends Component {
     const {restaurantId, reviews, deleteReview} = this.props
     const relevantReviews = reviews.filter( review => review.restaurantId === restaurantId)
     
-    const list = relevantReviews.map( (r,idx) => <Review key={idx} review={r} deleteReview={deleteReview} /> )
-    
     return (
       <ul>
-        {/* { relevantReviews.map( (r,idx) => <Review key={idx} review={r} deleteReview={deleteReview} /> ) } */}
-        {list}
+        { relevantReviews.map( (r,idx) => <Review key={idx} review={r} deleteReview={deleteReview} /> ) }
       </ul>
     );
   }
