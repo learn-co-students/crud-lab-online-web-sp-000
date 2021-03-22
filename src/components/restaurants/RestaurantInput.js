e7f44a6a8bf220e412a3ec6025088d59f61720a3
Sarah Condon <s.j.condon01@gmail.com>
2021-03-21 19:17:23 -0600

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+  	text: '' // the form field is initially blank, hence the empty string
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({ text: ''}) // clear the form after its submission
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <label>Add a Restaurant:</label>
+          <input type="text" value={this.state.text} onChange={this.handleOnChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
