78bf290d72697183de23af12958074fcfb503228
nllew <n.lewis.perc@gmail.com>
2021-04-06 18:54:35 -0500

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+
+  }
+
   render() {
     return (
       <div>
         Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="text" onChange={this.handleChange} />
+          <button type="submit"> Submit </button>
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const linkedReviews = reviews.filter(review => review.restaurantId === restaurantId);
+
+    const reviewList = linkedReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviewList}
+        </ul>
+      </div>
     );
   }
+
 };
 
 export default Reviews;
