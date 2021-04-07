59fcffab99320d02c4ceaed447b632507b784b53
nllew <n.lewis.perc@gmail.com>
2021-04-06 19:49:02 -0500

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -32,27 +32,3 @@ export default function manageRestaurants(state = {
 
     }
 };
-
-// // 1) Restaurant Component with Redux
-// has a button that dispatches a DELETE_RESTAURANT action with the proper id when clicked:
-
-// AssertionError: expected 1 to equal 0
-// + expected - actual
-
-// -1
-// +0
-
-// at Context.<anonymous> (test/restaurants-test.js:127:52)
-// at processImmediate (node:internal/timers:464:21)
-
-// 2) Restaurant Component with Redux
-//  updates the state of the store to remove the component:
-
-// AssertionError: expected 2 to equal 1
-// + expected - actual
-
-// -2
-// +1
-
-// at Context.<anonymous> (test/restaurants-test.js:153:52)
-// at processImmediate (node:internal/timers:464:21)
